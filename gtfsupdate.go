6121a13e6c08cdeb455e5b3734db9b61824f4e0d
eddyionescu <eddyionescu@gmail.com>
2017-02-12 22:50:55 -0500

    Updates mongodb database

--- gtfsupdate.go
+++ gtfsupdate.go
@@ -1,37 +1,39 @@
-// AWS Lambda Service that updates stops-database every 12 hours
+// Go code for AWS Lambda Service that updates stops-database every 12 hours
 // by retrieving publicly-available GTFS data
+
+// Libraries used: mgo mongodb driver, gtfs parser, object-to-hash
 
 package main
 
 import (
 	"github.com/geops/gtfsparser"
-	_ "github.com/lib/pq"
 	"fmt"
 	"os"
 	"log"
 	"net/http"
 	"io"
-	"database/sql"
 	"io/ioutil"
+	_ "gopkg.in/mgo.v2/bson"
+	_ "container/list"
+	"gopkg.in/mgo.v2"
+	"strings"
+	_ "github.com/go-mgo/mgo/bson"
+	"github.com/mitchellh/hashstructure"
+	"github.com/go-mgo/mgo/bson"
+	"strconv"
 )
 
 func getZip(url string, name string) {
-	// Create output file
+
 	newFile, err := os.Create(name)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer newFile.Close()
 
-	// HTTP GET request devdungeon.com
 	response, err := http.Get(url)
 	defer response.Body.Close()
 
-	// Write bytes from HTTP response to file.
-	// response.Body satisfies the reader interface.
-	// newFile satisfies the writer interface.
-	// That allows us to use io.Copy which accepts
-	// any type that implements reader and writer interface
 	numBytesWritten, err := io.Copy(newFile, response.Body)
 	if err != nil {
 		log.Fatal(err)
@@ -40,15 +42,28 @@ func getZip(url string, name string) {
 }
 
 func getdbpass() string {
-	b, err := ioutil.ReadFile(".pass")
+	b, err := ioutil.ReadFile(".mongopass")
 	if err != nil {
 		fmt.Print(err)
 	}
-	return string(b) // convert content to a 'string'
+	return strings.TrimRight(string(b), "\n")
+}
+
+type location struct {
+	Type string `json:"type"`
+	Coordinates []float32 `json:"coordinates"`
+}
+
+type stop struct {
+	Id bson.ObjectId `bson:"_id"`
+	Name string `json:"name"`
+	LocalId string `json:"localid"`
+	Location location `json:"location"`
 }
 
 func main() {
 
+	// get gtfs from waterloo
 	feed := gtfsparser.NewFeed()
 	grtLink := "http://www.regionofwaterloo.ca/opendatadownloads/GRT_GTFS.zip"
 	grtName := "./src/grt.zip"
@@ -58,20 +73,60 @@ func main() {
 	fmt.Printf("Done, parsed %d agencies, %d stops, %d routes, %d trips, %d fare attributes\n\n",
 	len(feed.Agencies), len(feed.Stops), len(feed.Routes), len(feed.Trips), len(feed.FareAttributes))
 
-	for k, v := range feed.Stops {
-		fmt.Printf("[%s] %s (@ %f,%f)\n", k, v.Name, v.Lat, v.Lon)
-	}
+	// connect to mongodb
+	info := mgo.DialInfo{}
+	info.Addrs = []string{"ec2-52-60-121-211.ca-central-1.compute.amazonaws.com"}
+	info.Database = "transistops"
+	info.Username = "eddy"
+	info.Password = getdbpass()
+	info.Mechanism = "SCRAM-SHA-1"
+	session, err := mgo.DialWithInfo(&info)
 
-	db, err := sql.Open("postgres", "postgres://eddy:" + getdbpass() +
-		"@transistops.cchubqlyefe9.ca-central-1.rds.amazonaws.com/transistopsdb?sslmode=disable")
-	 _, err1 := db.Exec("INSERT INTO stops (stop) VALUES (123)")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+	defer session.Close()
+
+
+	// iterate through stops and update database
+	stops := make([]stop, len(feed.Stops))
+	i := 0
+	agency := ""
+	for _, a := range feed.Agencies {
+		agency += a.Name + "_"
+	}
+	for k, v := range feed.Stops {
+		stops[i] = (stop{
+			Name: v.Name,
+			LocalId: k,
+			Location: location{"Point", []float32{v.Lon, v.Lat}},
+		})
+		hash, err := hashstructure.Hash(stops[i], nil)
+		if err != nil {
+			panic(err)
+		}
+		stops[i].Id = bson.ObjectId(strconv.FormatUint(hash, 10)[0:12])
+		//fmt.Printf("[%s] %s (@ %f,%f)\n", k, v.Name, v.Lat, v.Lon)
+		fmt.Println(hash)
+		fmt.Println(stops[i].Id)
+		fmt.Println(stops[i].Name)
+		i++
+	}
+
+	collection := session.DB("transistops").C("stops")
+
+	// TODO update instead of dropping
+	collection.DropCollection()
+	bulk := collection.Bulk()
+	for _, stop := range stops {
+		fmt.Println(stop.Id)
+		fmt.Println(stop.Name)
+		if(stop.Id != "") {
+			bulk.Insert(stop)
+		}
+	}
+	_, err1 := bulk.Run()
 	if err1 != nil {
-		log.Fatal(err1)
+		panic(err1)
 	}
-
-
-	//rows, err := db.Query("SELECT name FROM users WHERE age = $1", age)
 }
